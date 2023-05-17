0395bbdab4dc21625575d51f5eda8d63518aa463
email <bethkimaku@gmail.com>
2023-05-17 20:28:11 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    if length == 1:
+        print(a)
+        else:
+            print(a)
+            print(b)
+            for i in range(2,n):
+                c = a + b
+                a = b
+                b = c
+                print(c)
+print_fibonacci(9)           
